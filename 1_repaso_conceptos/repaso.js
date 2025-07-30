30c1c54ed7b69dde82bf2b9cd6b806ba093b64df
Valentina-Atehortua <vatehortua228@gmail.com>
2025-07-30 15:27:54 -0500

    aplicacion terminada

--- 1_repaso_conceptos/repaso.js
+++ 1_repaso_conceptos/repaso.js
@@ -1,14 +1,26 @@
-let img = document.querySelector("#img1")
-let butt = document.querySelector("#blanco")
-let butt2 = document.querySelector("#desenfocar")
-let butt3 = document.querySelector("#zoom")
+const pantalla = document.querySelector("#img1")
+const btn1 = document.querySelector("#blanco")
+const btn2 = document.querySelector("#desenfocar")
+const btn3 = document.querySelector("#zoom")
 
-img.addEventListener("click", () => {
-    img.style.filter = "grayscale(100%)";
-});
-butt.addEventListener("click", () => {
-    img.style.filter = "grayscale(100%)";
-});
-butt2.addEventListener("click", () => {
-    img.style.filter = "blur(2px)";
-});
+function blancoynegro () {
+    console.log ("Imagen modificada")
+    pantalla.style.filter = "grayscale(100%)"
+    /* btn1.style.backgroundColor = "orange"
+    btn1.style.color = "white"
+    btn1.style.height = "200px"
+    btn1.style.borderRadius = "50%" */
+}
+btn1.addEventListener("click", blancoynegro);
+
+function desenfocar () {
+    console.log ("Imagen modificada")
+    pantalla.style.filter = "blur(5px)"
+}
+btn2.addEventListener("click", desenfocar);
+
+function zoom () {
+    console.log ("Imagen modificada")
+    pantalla.style.transform = "scale(1.2) rotate(25deg)"
+}
+btn3.addEventListener("click", zoom);
