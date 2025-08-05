52c93baf93ff4f5e90325c2082f86f852707e01b
Valentina-Atehortua <vatehortua228@gmail.com>
2025-08-05 15:01:01 -0500

    Final

--- 2_poster_interactivo/app.js
+++ 2_poster_interactivo/app.js
@@ -1,13 +1,35 @@
 const monedas = document.querySelectorAll('.coin');
-const corazon = document.querySelector('.corazon');
+const corazones = document.querySelectorAll('.corazon');
 const contadorMonedas = document.querySelector('#contador-monedas');
 const contadorCorazones = document.querySelector('#contador-corazones');
+const movimiento = document.querySelector('.bloque1');
+const movi = document.querySelector('.bloque2');
+let totalMonedas = 0;
+let totalCorazones = 0;
 
-console.log(monedas,corazon,contadorMonedas,contadorCorazones);
-
+/* console.log(monedas,corazon,contadorMonedas,contadorCorazones);
+ */
 monedas.forEach(function (moneda){
     moneda.addEventListener( 'click', function () {
         moneda.classList.add('saltar')
+        totalMonedas++
+        contadorMonedas.textContent= totalMonedas
     })
     
+})
+
+corazones.forEach(function (corazon) {
+    
+    corazon.addEventListener( 'click', function () {
+        corazon.classList.add('saltar')
+        totalCorazones++
+        contadorCorazones.textContent = totalCorazones
+    })
+})
+movimiento.forEach(function (bloque1){
+    bloque1.classList.add('movimiento')
+})
+
+movi.forEach(function (bloque2){
+    bloque2.classList.add('movimiento1')
 })
