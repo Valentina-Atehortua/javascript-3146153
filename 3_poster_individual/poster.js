bd4ff5216c7d9660810b1e864229d706f1bc0a6a
Valentina-Atehortua <vatehortua228@gmail.com>
2025-08-12 16:06:59 -0500

    Terminado

---
+++ 3_poster_individual/poster.js
@@ -0,0 +1,39 @@
+let fox = document.querySelector('.foxy');
+let golde = document.querySelector('.golden');
+let sill = document.querySelector('.silla');
+let masc = document.querySelector('.mascara');
+let contadorInter = document.querySelector('#contador-interacciones');
+let totalinteraction = 0;
+
+fox.addEventListener('click', foxitranslate);
+function foxitranslate() {
+        fox.classList.add('mover')
+        totalinteraction++;
+        contadorInter.textContent = totalinteraction;
+
+        fox.addEventListener('click', foxitranslate);
+}
+golde.addEventListener('click', goldenscale);
+function goldenscale() {
+        golde.classList.add('mover2')
+        totalinteraction++;
+        contadorInter.textContent = totalinteraction;
+
+        golde.addEventListener('click', goldenscale);
+}
+sill.addEventListener('click', silltranslate);
+function silltranslate() {
+        sill.classList.add('mover4')
+        totalinteraction++;
+        contadorInter.textContent = totalinteraction;
+
+        sill.addEventListener('click', silltranslate);
+}
+masc.addEventListener('click', masrotate);
+function masrotate() {
+        masc.classList.add('mover3')
+        totalinteraction++;
+        contadorInter.textContent = totalinteraction;
+
+        masc.addEventListener('click', masrotate);
+}
