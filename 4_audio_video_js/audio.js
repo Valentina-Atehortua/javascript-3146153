b5c833da638954a2ba0493f349293a7703f8ec77
Valentina-Atehortua <vatehortua228@gmail.com>
2025-08-14 15:37:08 -0500

    empieza

---
+++ 4_audio_video_js/audio.js
@@ -0,0 +1,7 @@
+const video = document.querySelector("#video");
+const playVideo = document.querySelector("#play-video");
+const stopVideo = document.querySelector("#stop-video");
+const img1 = document.querySelector("#play-stop-img1");
+const img2 = document.querySelector("#play-stop-img2");
+
+console.log(video, playVideo, stopVideo, img1, img2);
