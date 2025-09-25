ebce98b50312fc0d4554267fcc313477ec3ddac2
Valentina-Atehortua <vatehortua228@gmail.com>
2025-09-25 17:00:13 -0500

    Avance

--- 6_Base-datos-comic/app.js
+++ 6_Base-datos-comic/app.js
@@ -11,8 +11,10 @@ comic.capitulos.forEach(cap => {
     const div = document.createElement("div")
     div.classList.add("card-miniatura")
     div.innerHTML = `
-         <img src="${cap.imgPortada}" data-index="0" alt="Miniatura 1">
-         <p>${cap.nombreCap}</p>
+    <a href="./capitulo.html?id=${cap.id}">
+        <img src="${cap.imgPortada}" data-index="0" alt="Miniatura 1">
+        <p>${cap.nombreCap}</p>
+    </a>
     `
     listaCaps.appendChild(div)
 })
